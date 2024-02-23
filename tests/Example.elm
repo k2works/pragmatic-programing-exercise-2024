0f745a7a7e6e853e0e97c96ba442da2f09b8bdf8
Katuyuki Kakigi <kakimomokuri@gmail.com>
2024-02-23 10:28:51 +0900

    test:文字列モジュール

--- tests/Example.elm
+++ tests/Example.elm
@@ -7,4 +7,27 @@ import Test exposing (..)
 
 suite : Test
 suite =
-    todo "Implement our first test. See https://package.elm-lang.org/packages/elm-explorations/test/latest for how to do this!"
+    describe "文字列モジュール"
+        [ describe "String.reverse"
+            [ test "回文には影響を与えない" <|
+                \() ->
+                    let
+                        palindrome =
+                            "hannah"
+                    in
+                        Expect.equal
+                            palindrome
+                            (String.reverse palindrome)
+            , test "既知の文字列を反転" <|
+                \() ->
+                    "ABCDEFG"
+                        |> String.reverse
+                        |> Expect.equal "GFEDCBA"
+            , fuzz string "もう一度実行すると元の文字列を復元" <|
+                \randomlyGeneratedString ->
+                    randomlyGeneratedString
+                        |> String.reverse
+                        |> String.reverse
+                        |> Expect.equal randomlyGeneratedString
+            ]
+        ]
