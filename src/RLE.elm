2aa7eb55193401334143c99812a1b347bde4849c
k2works <kakimomokuri@gmail.com>
2024-03-02 16:39:24 +0900

    test: らしいコード

--- src/RLE.elm
+++ src/RLE.elm
@@ -3,23 +3,14 @@ module RLE exposing (..)
 
 rle : String -> String
 rle str =
-    case String.uncons str of
-        Nothing ->
-            ""
-
-        Just ( h, t ) ->
-            aux 1 h t h
+    (fromCharAndRunLength << toCharAndRunLength) str
 
 
-aux : Int -> Char -> String -> Char -> String
-aux runLength prevChar str currentChar =
-    case String.uncons str of
-        Nothing ->
-            String.fromChar prevChar ++ String.fromInt runLength
+fromCharAndRunLength : List ( Char, Int ) -> String
+fromCharAndRunLength =
+    Debug.todo "Implement me!"
 
-        Just ( c, s ) ->
-            if c == prevChar then
-                aux (runLength + 1) prevChar s currentChar
 
-            else
-                String.fromChar prevChar ++ String.fromInt runLength ++ aux 1 c s c
+toCharAndRunLength : String -> List ( Char, Int )
+toCharAndRunLength =
+    Debug.todo "Implement me!"
