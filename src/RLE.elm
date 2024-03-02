303195b04276c8b25916fa0094b9dbaea3b74d25
k2works <kakimomokuri@gmail.com>
2024-03-02 17:52:38 +0900

    test: らしいコード

--- src/RLE.elm
+++ src/RLE.elm
@@ -1,42 +1,72 @@
 module RLE exposing (..)
 
+-- 文字列のランレングスエンコーディング
+
 
 rle : String -> String
 rle str =
-    let
-        groupAndCount list =
+    str
+        |> String.toList
+        |> groupAndCount
+        |> List.map pairToString
+        |> String.concat
+
+
+
+-- 連続する同一の要素をグループ化し、その出現数をカウントする
+
+
+groupAndCount : List Char -> List ( Char, Int )
+groupAndCount list =
+    list
+        |> group
+        |> List.map (\subList -> ( List.head subList |> Maybe.withDefault ' ', List.length subList ))
+
+
+
+-- 連続する同一の要素をグループ化する
+
+
+group : List Char -> List (List Char)
+group list =
+    case list of
+        [] ->
+            []
+
+        x :: xs ->
             let
-                grouped =
-                    group list
+                ( ys, zs ) =
+                    span ((==) x) xs
             in
-            List.map (\subList -> ( List.head subList |> Maybe.withDefault ' ', List.length subList )) grouped
+            (x :: ys) :: group zs
 
-        group list =
-            case list of
-                [] ->
-                    []
 
-                x :: xs ->
-                    let
-                        ( ys, zs ) =
-                            span ((==) x) xs
-                    in
-                    (x :: ys) :: group zs
 
-        span predicate list =
-            case list of
-                [] ->
-                    ( [], [] )
+-- プレディケートが初めてFalseを返す地点でリストを二つに分割する
 
-                x :: xs ->
-                    if predicate x then
-                        let
-                            ( ys, zs ) =
-                                span predicate xs
-                        in
-                        ( x :: ys, zs )
 
-                    else
-                        ( [], list )
-    in
-    (String.concat << List.map (\( c, n ) -> String.fromChar c ++ String.fromInt n) << groupAndCount << String.toList) str
+span : (a -> Bool) -> List a -> ( List a, List a )
+span predicate list =
+    case list of
+        [] ->
+            ( [], [] )
+
+        x :: xs ->
+            if predicate x then
+                let
+                    ( ys, zs ) =
+                        span predicate xs
+                in
+                ( x :: ys, zs )
+
+            else
+                ( [], list )
+
+
+
+-- 文字とカウントのペアを文字列に変換する
+
+
+pairToString : ( Char, Int ) -> String
+pairToString ( c, n ) =
+    String.fromChar c ++ String.fromInt n
