a2bc9e871f18bf0cc0e29bced4e2ccbfc8f26ef8
Katuyuki Kakigi <kakimomokuri@gmail.com>
2024-03-11 17:10:40 +0900

    build:Webサーバ1台のデプロイ

--- src/main.tf
+++ src/main.tf
@@ -7,7 +7,25 @@ resource "aws_instance" "example" {
   ami           = "ami-039e8f15ccb15368a"
   instance_type = "t2.micro"
 
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World" > index.html
+              nohup busybox httpd -f -p 8080 &
+              EOF
+
+  user_data_replace_on_change = true
+
   tags = {
     Name = "terraform-example"
+  }
+}
+
+resource "aws_security_group" "instance" {
+  name_prefix = "terraform-example-instance"
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
