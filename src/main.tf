ea8fc597b33782e7de8c11d397ee00ff418d0f96
Katuyuki Kakigi <kakimomokuri@gmail.com>
2024-03-11 17:45:00 +0900

    build:設定変更可能なWebサーバのデプロイ

--- src/main.tf
+++ src/main.tf
@@ -3,6 +3,12 @@ provider "aws" {
   profile = "k2works-poc-202402"
 }
 
+variable "server_port" {
+  description = "The port the server will use for HTTP requests"
+  type        = number
+  default     = 8080
+}
+
 resource "aws_instance" "example" {
   ami           = "ami-039e8f15ccb15368a"
   instance_type = "t2.micro"
@@ -10,7 +16,7 @@ resource "aws_instance" "example" {
   user_data = <<-EOF
                 #!/bin/bash
                 echo "Hello, World" > index.html
-                nohup python3 -m http.server 8080 &
+                nohup python3 -m http.server ${var.server_port} &
                 EOF
 
   user_data_replace_on_change = true
@@ -28,4 +34,9 @@ resource "aws_security_group" "instance" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+}
+
+output "public_ip" {
+  value = aws_instance.example.public_ip
+  description = "The public IP of the web server"
 }
