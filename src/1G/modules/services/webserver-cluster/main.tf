d031400ea425c525b8439d9719863952d8b537f9
Katuyuki Kakigi <kakimomokuri@gmail.com>
2024-03-12 17:58:05 +0900

    build:モジュールのローカルな値

--- src/1G/modules/services/webserver-cluster/main.tf
+++ src/1G/modules/services/webserver-cluster/main.tf
@@ -10,6 +10,14 @@ provider "aws" {
   profile = "k2works-poc-202402"
 }
 
+locals {
+  http_port    = 80
+  any_port     = 0
+  any_protocol = "-1"
+  tcp_protocol = "tcp"
+  all_ips      = ["0.0.0.0/0"]
+}
+
 resource "aws_launch_configuration" "example" {
   image_id      = "ami-039e8f15ccb15368a"
   instance_type = var.instance_type
@@ -68,7 +76,7 @@ resource "aws_lb" "example" {
 
 resource "aws_lb_listener" "http" {
   load_balancer_arn = aws_lb.example.arn
-  port              = 80
+  port              = local.http_port
   protocol          = "HTTP"
 
   default_action {
@@ -119,17 +127,17 @@ resource "aws_security_group" "alb" {
   name = "${var.cluster_name}-alb"
 
   ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    from_port   = local.http_port
+    to_port     = local.http_port
+    protocol    = local.tcp_protocol
+    cidr_blocks = local.all_ips
   }
 
   egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    from_port   = local.any_port
+    to_port     = local.any_port
+    protocol    = local.any_protocol
+    cidr_blocks = local.all_ips
   }
 }
 
