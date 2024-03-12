011926919f896eac16d6a5a1070a5a61b35af4ec
Katuyuki Kakigi <kakimomokuri@gmail.com>
2024-03-12 13:57:07 +0900

    build:ファイルステートの共有ストレージ

--- src/main.tf
+++ src/main.tf
@@ -3,6 +3,52 @@ provider "aws" {
   profile = "k2works-poc-202402"
 }
 
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "k2works-poc-202402-terraform-state"
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "aws_s3_bucket_versioning" "enabled" {
+  bucket = aws_s3_bucket.terraform_state.bucket
+
+  versioning_configuration {
+    status  = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "public_access" {
+  bucket = aws_s3_bucket.terraform_state.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_dynamodb_table" "terraform_locks" {
+  name = "k2works-poc-202402-terraform-state-locks"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
+
+terraform {
+  backend "s3" {
+    bucket = "k2works-poc-202402-terraform-state"
+    key    = "global/s3/terraform.tfstate"
+    region = "ap-northeast-1"
+    dynamodb_table = "k2works-poc-202402-terraform-state-locks"
+    encrypt = true
+    profile = "k2works-poc-202402"
+  }
+}
+
 variable "server_port" {
   description = "The port the server will use for HTTP requests"
   type        = number
@@ -142,4 +188,14 @@ resource "aws_lb_listener_rule" "asg" {
 output "alb_dns_name" {
   value       = aws_lb.example.dns_name
   description = "The domain name of the load balancer"
+}
+
+output "s3_bucket_arn" {
+  value = aws_s3_bucket.terraform_state.arn
+  description = "The ARN of the S3 bucket"
+}
+
+output "dynamodb_table_name" {
+  value = aws_dynamodb_table.terraform_locks.name
+  description = "The name of the DynamoDB table"
 }
