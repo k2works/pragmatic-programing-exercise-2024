b39de53b52b6e0956326353d62aff84ae97b7ae5
Katuyuki Kakigi <kakimomokuri@gmail.com>
2024-03-13 13:59:25 +0900

    build:ゼロダウンタイムデプロイ

--- src/1G/modules/services/webserver-cluster/main.tf
+++ src/1G/modules/services/webserver-cluster/main.tf
@@ -34,21 +34,20 @@ resource "aws_launch_configuration" "example" {
 }
 
 resource "aws_autoscaling_group" "example" {
-  name = "${var.cluster_name}-${aws_launch_configuration.example.name}"
-
+  name                 = var.cluster_name
   launch_configuration = aws_launch_configuration.example.name
   vpc_zone_identifier  = data.aws_subnets.default.ids
-
-  target_group_arns = [aws_lb_target_group.asg.arn]
-  health_check_type = "ELB"
+  target_group_arns    = [aws_lb_target_group.asg.arn]
+  health_check_type    = "ELB"
 
   min_size = var.min_size
   max_size = var.max_size
 
-  min_elb_capacity = var.min_size
-
-  lifecycle {
-    create_before_destroy = true
+  instance_refresh {
+    strategy = "Rolling"
+    preferences {
+      min_healthy_percentage = 50
+    }
   }
 
   tag {
