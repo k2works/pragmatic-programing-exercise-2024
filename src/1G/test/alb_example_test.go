46ea8655d38d3d345d538c64005ad8775ea41be5
Katuyuki Kakigi <kakimomokuri@gmail.com>
2024-03-18 16:58:56 +0900

    test:Terraformコードのユニットテスト

---
+++ src/1G/test/alb_example_test.go
@@ -0,0 +1,37 @@
+package test
+
+import (
+	"fmt"
+	http_helper "github.com/gruntwork-io/terratest/modules/http-helper"
+	"github.com/gruntwork-io/terratest/modules/terraform"
+	"testing"
+	"time"
+)
+
+func TestAlbExample(t *testing.T) {
+	opts := &terraform.Options{
+		TerraformDir: "../examples/alb",
+	}
+
+	defer terraform.Destroy(t, opts)
+
+	terraform.InitAndApply(t, opts)
+
+	albDnsName := terraform.OutputRequired(t, opts, "alb_dns_name")
+	url := fmt.Sprintf("http://%s", albDnsName)
+
+	expectedStatus := 404
+	expectedBody := "404: page not found"
+	maxRetries := 10
+	timeBetweenRetries := 10 * time.Second
+
+	http_helper.HttpGetWithRetry(
+		t,
+		url,
+		nil,
+		expectedStatus,
+		expectedBody,
+		maxRetries,
+		timeBetweenRetries,
+	)
+}
