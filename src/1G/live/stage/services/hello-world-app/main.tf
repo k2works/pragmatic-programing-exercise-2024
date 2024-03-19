eeb4a3ffddf84fb5840c24e393795c38dbdb7b17
Katuyuki Kakigi <kakimomokuri@gmail.com>
2024-03-19 12:21:16 +0900

    fix:統合テスト

--- src/1G/live/stage/services/hello-world-app/main.tf
+++ src/1G/live/stage/services/hello-world-app/main.tf
@@ -38,18 +38,6 @@ data "aws_ami" "amazonlinux" {
 
   filter {
     name   = "name"
-    values = ["amzn-ami-hvm-*"]
-  }
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-  filter {
-    name   = "root-device-type"
-    values = ["ebs"]
-  }
-  filter {
-    name   = "architecture"
-    values = ["x86_64"]
+    values = ["al2023-ami-*"]
   }
 }
